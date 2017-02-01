e11cbd4e9ab31c68b148a081fb8281efdb4054f9
Peter Kim <peterkim23@gmail.com>
2017-02-01 11:34:14 -0800

    added delete pic route. added stying to match mockup

--- app/service/pic-service.js
+++ app/service/pic-service.js
@@ -39,5 +39,34 @@ function picService($q, $log, $http, Upload, authService) {
     });
   };
 
+  service.deletePic = function(galleryData, picData) {
+    $log.debug('picService.deletePic');
+
+    return authService.getToken()
+    .then( token => {
+      let url = `${__API_URL__}/api/gallery/${galleryData._id}/pic/${picData._id}`;
+      let config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      };
+
+      return $http.delete(url, config);
+    })
+    .then( res => {
+      for (let i = 0; i < galleryData.pics.length; i++) {
+        // let current = galleryData.picData[i];
+        if (galleryData.pics[i]._id === picData._id) {
+          galleryData.pics.splice(i, 1);
+          break;
+        }
+      }
+    })
+    .catch( err => {
+      $log.error(err.message);
+      return $q.reject(err);
+    });
+  };
+
   return service;
 }
