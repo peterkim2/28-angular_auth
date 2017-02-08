75de8b94f95b29cb195195208ead0ccc4bb4c6ad
Peter Kim <peterkim23@gmail.com>
2017-02-07 19:17:32 -0800

    added social icons directive

--- app/entry.js
+++ app/entry.js
@@ -38,3 +38,10 @@ context.keys().forEach( key => {
   let module = context(key);
   cfgram.component(name, module);
 });
+
+context = require.context('./directive/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  cfgram.directive(name, module);
+});
